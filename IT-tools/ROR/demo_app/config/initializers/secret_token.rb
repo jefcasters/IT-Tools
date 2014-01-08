660b56d33b276b599c8441fb252d01b272412453
Casters Jef <1136251@student.khlim.be>
2014-01-08 15:37:41 +0100

    laatste commit

---
+++ IT-tools/ROR/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2ae11ada688a13781f4c27d8909c5b25daafd344228a6e37a3c965dfa85b536e11a054909efab550aaa223dabc129eb901048be49fa79eea37522923bce5a403'
